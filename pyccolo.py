f6e7658ae4c4d8f4f49fe98d0e9651e74cbc6408
Doug Sheffer <sheffer@nscl.msu.edu>
2012-11-21 16:44:18 -0500

    Implemented graphical user interface.

--- pyccolo.py
+++ pyccolo.py
@@ -17,44 +17,55 @@ this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
 
+from pandora import Pandora
+import pygst
+pygst.require('0.10')
+import gst
+import threading
+
+import pygtk
+pygtk.require('2.0')
+import gtk
+import gobject
+import cairo
+
+#import RPi.GPIO as GPIO
 import ConfigParser
 import time
 import urllib2
 
-import gobject
-import pygst
-pygst.require("0.10")
-import gst
-import threading
-
-import RPi.GPIO as GPIO
-
-from pandora import *
-
-import pygtk
-pygtk.require("2.0")
-import gtk
-import cairo
-
-CONF_FILE = "/etc/pyccolo/pyccolo.conf"
+CONF_FILE = '/etc/pyccolo/pyccolo.conf'
 PIN_CCW = 23
 PIN_CW = 24
 PIN_CLICK = 25
 
-class Display(gtk.Window):
+SCREEN_WIDTH = 480
+SCREEN_HEIGHT = 272
+ALBUM_ART_SIZE = 96
+
+# __        ___           _
+# \ \      / (_)_ __   __| | _____      __
+#  \ \ /\ / /| | '_ \ / _` |/ _ \ \ /\ / /
+#   \ V  V / | | | | | (_| | (_) \ V  V /
+#    \_/\_/  |_|_| |_|\__,_|\___/ \_/\_/
+#
+
+class Window(gtk.Window):
     def __init__(self):
-        super(Display, self).__init__()
+        """Initialize graphics."""
+
+        gtk.Window.__init__(self)
 
     def do_realize(self):
         """Initialize a place to draw the GUI."""
 
         self.set_flags(self.flags() | gtk.REALIZED)
-        self.connect("delete-event", gtk.main_quit)
+        self.connect('delete-event', gtk.main_quit)
 
         self.window = gtk.gdk.Window(
             self.get_parent_window(),
-            width = self.get_screen().get_width(),
-            height = self.get_screen().get_height(),
+            width = SCREEN_WIDTH, #self.get_screen().get_width(),
+            height = SCREEN_HEIGHT, #self.get_screen().get_height(),
             window_type = gtk.gdk.WINDOW_TOPLEVEL,
             wclass = gtk.gdk.INPUT_OUTPUT,
             event_mask = self.get_events() | gtk.gdk.EXPOSURE_MASK
@@ -65,60 +76,206 @@ class Display(gtk.Window):
         self.set_default_size(w, h)
 
         self.style.attach(self.window)
-        self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse("red"))
-        self.style.set_background(self.window, gtk.STATE_NORMAL)
+        #self.modify_bg(gtk.STATE_NORMAL, gtk.gdk.color_parse('black'))
+        #self.style.set_background(self.window, gtk.STATE_NORMAL)
         self.window.set_user_data(self)
 
-class Pyccolo:
-    def __init__(self, display):
+#  ____  _           _
+# |  _ \(_)___ _ __ | | __ _ _   _
+# | | | | / __| '_ \| |/ _` | | | |
+# | |_| | \__ \ |_) | | (_| | |_| |
+# |____/|_|___/ .__/|_|\__,_|\__, |
+#             |_|            |___/
+
+class Display(gtk.DrawingArea):
+    def __init__(self):
+        """Initialize graphical user interface."""
+
+        gtk.DrawingArea.__init__(self)
+
+        self.mode = None
+        self.stations = []
+        self.station = None
+        self.artist = ''
+        self.album = ''
+        self.track = ''
+        self.art_url = ''
+        self.art = None
+        self.playing = False
+
+        self.bg = gtk.gdk.pixbuf_new_from_file('background.png')
+
+        # Create a container window.
+        window = Window()
+        window.add(self)
+        window.show_all()
+
+        self.connect('expose-event', self.expose)
+
+    def expose(self, widget, event):
+        cr = widget.window.cairo_create()
+        w = self.allocation.width
+        h = self.allocation.height
+
+        cr.set_source_rgb(0, 0, 0)
+        cr.paint()
+
+        if self.bg:
+            widget.window.draw_pixbuf(None, self.bg, dest_x=0, dest_y=0)
+
+        if not self.playing:
+            self.draw_text(cr, 240, 50, 'PAUSED', 24, align=0)
+
+        if self.mode == Controller.MODE_STATION:
+            self.draw_text(cr, 240, 250, '<- Station ->', 16, align=0)
+
+        if self.station:
+            station = self.stations[self.station]
+            self.draw_text(cr, 240, 225, station, 16, align=0)
+
+        if self.track:
+            self.draw_text(cr, 240, 40, self.track, 20, align=0)
+            self.draw_text(cr, 220, 100, 'by', 10, align=-1)
+            self.draw_text(cr, 225, 100, self.artist, 16,
+                           weight=cairo.FONT_WEIGHT_BOLD)
+            self.draw_text(cr, 220, 125, 'from', 10, align=-1)
+            self.draw_text(cr, 225, 125, self.album, 14)
+
+        if self.art:
+            widget.window.draw_pixbuf(None, self.art, dest_x=75, dest_y=75)
+
+    def draw_text(self, cr, x, y, text, size, r=1, g=1, b=1, a=1,
+                  face='Ubuntu', align=1,
+                  slant=cairo.FONT_SLANT_NORMAL,
+                  weight=cairo.FONT_WEIGHT_NORMAL):
+        cr.save()
+        cr.select_font_face(face, slant, weight)
+        cr.set_font_size(size)
+        cr.set_source_rgba(r, g, b, a)
+        if align == 0:
+            x = x - cr.text_extents(text)[2] / 2
+        elif align == -1:
+            x = x - cr.text_extents(text)[2]
+        cr.move_to(x, y)
+        cr.text_path(text)
+        cr.clip()
+        cr.paint()
+        cr.restore()
+
+    def load_art(self):
+        self.art = None
+
+        art_url = self.art_url
+
+        content = urllib2.urlopen(art_url).read()
+        loader = gtk.gdk.PixbufLoader()
+        loader.set_size(ALBUM_ART_SIZE, ALBUM_ART_SIZE)
+        loader.write(content)
+        loader.close()
+
+        if self.art_url == art_url:
+            self.art = loader.get_pixbuf()
+        self.queue_draw()
+
+    def change_mode(self, controller, mode):
+        """Change the current user interface control mode."""
+
+        self.mode = mode
+        self.queue_draw()
+
+    def change_station(self, music, station, stations):
+        """Change the station list that is displayed."""
+
+        self.stations = stations
+        self.station = station
+        self.queue_draw()
+
+    def change_song(self, music, artist, album, track, art):
+        """Change the song name that is displayed."""
+
+        self.artist = artist
+        self.album = album
+        self.track = track
+        self.art_url = art
+        threading.Thread(target=self.load_art).start()
+        self.queue_draw()
+
+    def change_state(self, music, state):
+        """Change the playing state that is displayed."""
+
+        self.playing = state
+        self.queue_draw()
+
+#  __  __           _
+# |  \/  |_   _ ___(_) ___
+# | |\/| | | | / __| |/ __|
+# | |  | | |_| \__ \ | (__
+# |_|  |_|\__,_|___/_|\___|
+#
+
+class Music(gobject.GObject):
+    __gsignals__ = {
+        'station-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE,
+                            (gobject.TYPE_STRING, gobject.TYPE_PYOBJECT,)),
+        'song-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE,
+                         (gobject.TYPE_STRING, gobject.TYPE_STRING,
+                          gobject.TYPE_STRING, gobject.TYPE_STRING,)),
+        'state-changed': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE,
+                          (gobject.TYPE_BOOLEAN,)),
+        }
+
+    def __init__(self):
+        """Initialize and play Pandora radio stations."""
+
+        gobject.GObject.__init__(self)
+
         self.station = None
         self.playlists = dict()
         self.song = None
         self.playing = False
         self.timer = None
 
-        # Read in configuration details.
-        self.config = ConfigParser.ConfigParser()
-        self.config.read(CONF_FILE)
-        try:
-            username = self.config.get("User", "username")
-            password = self.config.get("User", "password")
-        except:
-            print "Failed to load username and password from configuration file."
-            exit(1)
-
         # Initialize Pandora.
         self.pandora = Pandora()
-        self.pandora.connect(username, password)
 
         # Initialize Gstreamer.
-        self.player = gst.element_factory_make("playbin2", "player")
+        self.player = gst.element_factory_make('playbin2', 'player')
         self.player.props.flags |= 0x80 # GST_PLAY_FLAG_DOWNLOAD (progressive download)
         bus = self.player.get_bus()
         bus.add_signal_watch()
-        bus.connect("message::eos", self.on_gst_eos)
-        bus.connect("message::buffering", self.on_gst_buffering)
-        bus.connect("message::error", self.on_gst_error)
+        bus.connect('message::eos', self.on_gst_eos)
+        bus.connect('message::error', self.on_gst_error)
+
+    def init(self, username=None, password=None):
+        self.config = ConfigParser.ConfigParser()
+        self.config.read(CONF_FILE)
+
+        if not username or not password:
+            # Read in configuration details.
+            try:
+                username = self.config.get('User', 'username')
+                password = self.config.get('User', 'password')
+            except:
+                print 'Failed to load username and password from configuration file.'
+                exit(1)
+
+        try:
+            self.pandora.connect(username, password)
+        except (PandoraTimeout, PandoraNetError) as e:
+            return False
 
         # Attempt to restore last station.
         self.pandora.get_stations()
         try:
-            last_station_id = self.config.get("Station", "station_id")
+            last_station_id = self.config.get('Station', 'station_id')
         except:
             last_station_id = self.pandora.stations[0].id
         self.set_station(last_station_id)
 
-    def get_station_id(self):
-        """Get the id of the current station."""
-
-        if not self.station:
-            return None
-        return self.station.id
+        return True
 
     def set_station(self, station_id):
         """Set the current station."""
-
-        self.pause()
 
         # Change the station and the song.
         self.station = self.pandora.get_station_by_id(station_id)
@@ -131,19 +288,41 @@ class Pyccolo:
 
         # Save the new station into the configuration file.
         try:
-            if not self.config.has_section("Station"):
-                self.config.add_section("Station")
-            self.config.set("Station", "station_id", station_id)
-            with open(CONF_FILE, "wb") as config:
+            if not self.config.has_section('Station'):
+                self.config.add_section('Station')
+            self.config.set('Station', 'station_id', station_id)
+            with open(CONF_FILE, 'wb') as config:
                 self.config.write(config)
         except:
             pass
 
-        print "Station: %s" % self.station.name
+        stations = {}
+        for station in self.pandora.stations:
+            stations[station.id] = station.name
+        self.emit('station-changed', station_id, stations)
 
         return True
 
-    def tune_station(self, delta):
+    def play(self):
+        """Play the currently paused music track."""
+
+        self.player.set_state(gst.STATE_PLAYING)
+        if not self.playing:
+            self.playing = True
+            self.emit('state-changed', self.playing)
+
+        return True
+
+    def pause(self):
+        """Pause the currently playing music track."""
+
+        if self.playing:
+            self.player.set_state(gst.STATE_PAUSED)
+            self.playing = False
+            self.emit('state-changed', self.playing)
+        return True
+
+    def tune_station(self, controller, delta):
         """Change the station in one direction, like a tuning dial."""
 
         stations = self.pandora.stations
@@ -159,27 +338,15 @@ class Pyccolo:
 
         return self.set_station(stations[curr].id)
 
-    def is_playing(self):
-        """True if music is playing."""
-
-        return self.playing
-
-    def play(self):
-        """Play the currently paused music track."""
-
-        self.player.set_state(gst.STATE_PLAYING)
-        self.playing = True
-        return True
-
-    def pause(self):
-        """Pause the currently playing music track."""
+    def play_pause(self, controller):
+        """Toggle between playing and paused."""
 
         if self.playing:
-            self.player.set_state(gst.STATE_PAUSED)
-            self.playing = False
-        return True
+            self.pause()
+        else:
+            self.play()
 
-    def next_song(self):
+    def next_song(self, controller=None):
         """Skip the current music track."""
 
         if self.timer:
@@ -201,12 +368,11 @@ class Pyccolo:
         # Play the next song in the playlist.
         if station == self.station:
             self.song = playlist.pop(0)
-            self.player.set_property("uri", self.song.audioUrl)
+            self.player.set_property('uri', self.song.audioUrl)
             self.play()
 
-            print "> '%s' by '%s' from '%s'" % (self.song.title,
-                                                self.song.artist,
-                                                self.song.album)
+            self.emit('song-changed', self.song.artist, self.song.album,
+                      self.song.title, self.song.artRadio)
 
         self.playlists[station.id] = playlist
 
@@ -217,92 +383,111 @@ class Pyccolo:
 
         self.next_song()
 
-    def on_gst_buffering(self, bus, message):
-        """Display Gstreamer buffering progress to the user."""
-
-        #percent = message.parse_buffering()
-        pass
-
     def on_gst_error(self, bus, message):
-        """Display Gstreamer errors to the user."""
+        """Report Gstreamer errors."""
 
         self.playing = False
         err, debug = message.parse_error()
-        print "Gstreamer Error: %s, %s, %s" % (err, debug, err.code)
+        print 'Gstreamer Error: %s, %s, %s' % (err, debug, err.code)
         self.next_song()
 
-class Controller:
-    def __init__(self, pyccolo):
-        GPIO.setmode(GPIO.BCM)
+#   ____            _             _ _
+#  / ___|___  _ __ | |_ _ __ ___ | | | ___ _ __
+# | |   / _ \| '_ \| __| '__/ _ \| | |/ _ \ '__|
+# | |__| (_) | | | | |_| | | (_) | | |  __/ |
+#  \____\___/|_| |_|\__|_|  \___/|_|_|\___|_|
+#
 
-        GPIO.setup(PIN_CCW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+class Controller(gobject.GObject):
+    __gsignals__ = {
+        'change-mode': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE,
+                        (gobject.TYPE_INT,)),
+        'station-up': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+        'station-down': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+        'next-song': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+        'play-pause': (gobject.SIGNAL_RUN_LAST, gobject.TYPE_NONE, ()),
+        }
+
+    # Mode enumeration.
+    MODE_STATION = 1
+    MODE_VOLUME = 2
+
+    def __init__(self):
+        """Setup GPIO pins for input."""
+
+        gobject.GObject.__init__(self)
+
+        self.mode = Controller.MODE_STATION
         self.ccw = False
-
-        GPIO.setup(PIN_CW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self.cw = False
-
-        GPIO.setup(PIN_CLICK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
         self.click_time = None
 
-    def detect_events(self):
+    def main(self):
         """Process user interface events."""
 
-        # Counterclockwise rotation.
-        new_ccw = GPIO.input(PIN_CCW)
-        if new_ccw and not self.ccw:
-            pyccolo.tune_station(-1)
-        self.ccw = new_ccw
+        self.emit('change-mode', self.mode)
 
-        # Clockwise rotation.
-        new_cw = GPIO.input(PIN_CW)
-        if new_cw and not self.cw:
-            pyccolo.tune_station(1)
-        self.cw = new_cw
+        return
 
-        # Button pressed.
-        new_click = GPIO.input(PIN_CLICK)
-        if new_click and not self.click_time:
-            self.click_time = time.time()
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(PIN_CCW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(PIN_CW, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+        GPIO.setup(PIN_CLICK, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-        # Button release.
-        if not new_click and self.click_time:
-            click_duration = time.time() - self.click_time
-            self.click_time = None
+        while True:
+            # Counterclockwise rotation.
+            new_ccw = GPIO.input(PIN_CCW)
+            if new_ccw and not self.ccw:
+                self.emit('station-down')
+            self.ccw = new_ccw
 
-            if pyccolo.is_playing():
-                pyccolo.pause()
-            else:
-                pyccolo.play()
+            # Clockwise rotation.
+            new_cw = GPIO.input(PIN_CW)
+            if new_cw and not self.cw:
+                self.emit('station-up')
+            self.cw = new_cw
 
-        return True
+            # Button pressed.
+            # TODO: needs work
+            new_click = GPIO.input(PIN_CLICK)
+            if new_click:
+                if not self.click_time:
+                    self.click_time = time.time()
+                click_duration = time.time() - self.click_time
+                if click_duration > 1:
+                    self.emit('next-song')
+            # Button released.
+            elif not new_click and self.click_time:
+                click_duration = time.time() - self.click_time
+                if self.click_time < 1:
+                    self.emit('play-pause')
+                self.click_time = None
 
-def has_network():
-    """Determine if the Pandora website can be reached."""
+gobject.type_register(Window)
+gobject.type_register(Display)
+gobject.type_register(Controller)
+gobject.type_register(Music)
 
-    try:
-        response = urllib2.urlopen("http://pandora.com")
-        return True
-    except urllib2.URLError as err:
-        pass
-    return False
-
-if __name__ == "__main__":
-    gobject.type_register(Display)
-
+if __name__ == '__main__':
     display = Display()
-    display.show_all()
+    controller = Controller()
+    music = Music()
 
-    while not has_network():
+    # Connect signals between components.
+    controller.connect('change-mode', display.change_mode)
+    controller.connect('station-up', music.tune_station, 1)
+    controller.connect('station-down', music.tune_station, -1)
+    controller.connect('play-pause', music.play_pause)
+    controller.connect('next-song', music.next_song)
+    music.connect('station-changed', display.change_station)
+    music.connect('song-changed', display.change_song)
+    music.connect('state-changed', display.change_state)
+
+    while not music.init():
         time.sleep(1);
 
-    pyccolo = Pyccolo(display)
-    controller = Controller(pyccolo)
-
-    # Start main loop in a separate thread.
+    # Start controller in its own thread.
+    threading.Thread(target=controller.main).start()
     gtk.gdk.threads_init()
-    g_loop = threading.Thread(target=gtk.main)
-    g_loop.daemon = True
-    g_loop.start()
 
-    while controller.detect_events():
-        pass
+    gtk.main()
